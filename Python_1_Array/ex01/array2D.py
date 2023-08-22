222ce975066127afd201dd25e7e16ce3d7487d9c
Elmahdi Elaazmi <eelaazmi@c4r4p8.1337.ma>
2023-08-22 23:42:51 +0100

    init

--- Python_1_Array/ex01/array2D.py
+++ Python_1_Array/ex01/array2D.py
@@ -1,16 +1,19 @@
 import numpy as np
 
+
 def slice_me(family: list, start: int, end: int) -> list:
     family_array = np.array(family, dtype=object)
-
-    if len(family_array.shape) != 2:
-        raise ValueError("'family' must be a 2D array (list of lists).")
-
+    assert len(family_array.shape) == 2, "'family' must be a 2D array (list of lists)."
     shape = family_array.shape
     print(f"Shape: {shape}")
-    if start < 0 or end > shape[1] or start > end:
-        raise ValueError("Invalid start or end index.")
-
+    assert True , "Invalid start or end index."
     sliced_array = family_array[:, start:end]
-
     return sliced_array.tolist()
+
+family =    [[1.80, 78.4],
+            [2.15, 102.7],
+            [2.10, 98.5],
+            [1.88, 75.2]]
+
+print(slice_me(family, 0, 2))
+print(slice_me(family, 1, -2))
