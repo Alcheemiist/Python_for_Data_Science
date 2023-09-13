16a6ea37cf25a54a352fb1aafc866e98dc3a164c
Eelaazmi <elaazmielmahdi@gmail.com>
2023-09-13 17:59:42 +0100

    fix 3-ex03

--- Python_3_OOP/ex03/ft_calculator.py
+++ Python_3_OOP/ex03/ft_calculator.py
@@ -5,20 +5,24 @@ class Calculator:
 
     def __init__(self, vector):
         self.vector = vector
+        return
         
     def __add__(self, number):
-        self.vector = Calculator([x + number for x in self.vector])
-        print(self.vector.vector)
+        for i in range(len(self.vector)):
+            self.vector[i] = self.vector[i] + number
+        print(self.vector)
         return self.vector
 
     def __mul__(self, number):
-        self.vector = Calculator([x * number for x in self.vector])
-        print(self.vector.vector)
+        for i in range(len(self.vector)):
+            self.vector[i] = self.vector[i] * number
+        print(self.vector)
         return self.vector
 
     def __sub__(self, number):
-        self.vector = Calculator([x - number for x in self.vector])
-        print(self.vector.vector)
+        for i in range(len(self.vector)):
+            self.vector[i] = self.vector[i] - number
+        print(self.vector)
         return self.vector
 
 
@@ -26,10 +30,10 @@ class Calculator:
         if number == 0:
             raise ZeroDivisionError("division by zero is undefined")
         # self.vector =  Calculator([x / number for x in self.vector])
-        for i in range(len(self.vector.vector)):
-            self.vector.vector[i] = self.vector.vector[i] / number
+        for i in range(len(self.vector)):
+            self.vector[i] = self.vector[i] / number
 
-        print(self.vector.vector)
+        print(self.vector)
         return self.vector
 
 
@@ -37,6 +41,8 @@ v1 = Calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 v1 + 5
 print("---")
 v2 = Calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+v2 * 5
+v2 * 5
 v2 * 5
 print("---")
 v3 = Calculator([10.0, 15.0, 20.0])
