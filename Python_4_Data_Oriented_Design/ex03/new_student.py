864526b942c52fa10b76ba0732b34177d2be5804
Eelaazmi <elaazmielmahdi@gmail.com>
2023-09-14 07:21:33 +0100

    done day4

---
+++ Python_4_Data_Oriented_Design/ex03/new_student.py
@@ -0,0 +1,27 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    name: str
+    surname: str
+    active: bool = field(default=True)
+    login: str = field(default="")
+    id: str = field(default="")
+
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+        self.active = True
+        self.login = self.name[0].upper() + self.surname.lower()
+        self.id = generate_id()
+
+
+student = Student(name="Edward", surname="agle")
+print(student)
