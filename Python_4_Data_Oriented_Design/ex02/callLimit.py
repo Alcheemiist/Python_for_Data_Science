e92b020094a2a83018ad50e8d6821abee3b26a1e
Eelaazmi <elaazmielmahdi@gmail.com>
2023-09-14 06:53:46 +0100

    boya

---
+++ Python_4_Data_Oriented_Design/ex02/callLimit.py
@@ -0,0 +1,30 @@
+def callLimit(limit: int):
+    count = 0
+
+    def callLimiter(function):
+        def limit_function(*args: any, **kwds: any):
+            nonlocal count
+            if count >= limit:
+                print(f"Error: {function} call too many times")
+                return
+            count += 1
+            return function(*args, **kwds)
+
+        return limit_function
+
+    return callLimiter
+
+
+@callLimit(3)
+def f():
+    print("f()")
+
+
+@callLimit(1)
+def g():
+    print("g()")
+
+
+for i in range(3):
+    f()
+    g()
